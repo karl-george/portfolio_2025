776745a01c60d289e67abd2af0e0199c674503bd
Karl George <karlgeorge87@gmail.com>
2025-05-12 11:06:39 +0200

    Adjust padding to fit navbar better

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='relative py-6 md:py-8 text-center text-sm md:text-base text-muted-foreground mb-20'>
+    <footer className='relative py-10 md:py-8 text-center text-sm md:text-base text-muted-foreground mb-20'>
       <p>
         {' '}
         Made by <span className='text-white'>Karl George</span> using Nextjs ©
