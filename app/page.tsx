daf33bbb621a005ded6148616a1c2da5f0fa24f4
Karl George <karlgeorge87@gmail.com>
2025-05-12 11:04:16 +0200

    Clean up imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,11 @@
 
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
 import ParticlesBackground from '@/components/ParticlesBackground';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import Hero from '@/components/ui/Hero';
-import { skills } from '@/constants/data';
-import { Linkedin, Mail } from 'lucide-react';
 
 export default function Home() {
   return (
