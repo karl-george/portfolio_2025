3d46ad0dd90b6e1a27c66131fbf68dd356151a7b
Karl George <karlgeorge87@gmail.com>
2025-05-12 13:55:52 +0200

    Update details info

--- constants/data.ts
+++ constants/data.ts
@@ -1,27 +1,27 @@
 export const projects = [
   {
-    title: 'GroceryGo',
+    title: 'BlitzQuiz',
     description:
-      'GroceryGo is a mobile application designed to allow users to create and manage their shopping lists. Users can create and delete lists and then store and delete items in those lists',
-    image: '/images/shopping_list.png',
-    tech: ['React Native', 'Expo', 'SQLite', 'Drizzle', 'Sentry', 'NativeWind'],
-    link: '#',
+      'Blitz Quiz is a fast paced trivia game allowing users to answer various questions in an attempt to top the leaderboard. The app features rapid fire questions, multiple trivia categories and a leaderboard.',
+    image: '/images/quiz2.png',
+    tech: ['React Native', 'Expo', 'Supabase', 'Postgres', 'NativeWind'],
+    link: 'https://github.com/karl-george/blitz_quiz',
   },
   {
     title: 'GroceryGo',
     description:
-      'GroceryGo is a mobile application designed to allow users to create and manage their shopping lists. Users can create and delete lists and then store and delete items in those lists',
+      'GroceryGo is a mobile application designed to allow users to create and manage their shopping lists. Users can create and delete lists and then store and delete items in those lists.',
     image: '/images/shopping_list.png',
     tech: ['React Native', 'Expo', 'SQLite', 'Drizzle', 'Sentry', 'NativeWind'],
-    link: '#',
+    link: 'https://github.com/karl-george/shoppping_list',
   },
   {
-    title: 'GroceryGo',
+    title: 'CoinFlow',
     description:
-      'GroceryGo is a mobile application designed to allow users to create and manage their shopping lists. Users can create and delete lists and then store and delete items in those lists',
-    image: '/images/shopping_list.png',
-    tech: ['React Native', 'Expo', 'SQLite', 'Drizzle', 'Sentry', 'NativeWind'],
-    link: '#',
+      'CoinFlow allows users to track current market data of various crypto currencies. The app features a modern design and a user-friendly interface, making it easy for users to find and save their preffered crypto coins.',
+    image: '/images/cf-details.png',
+    tech: ['React Native', 'Expo', 'API', 'Clerk', 'Zustand'],
+    link: 'https://github.com/karl-george/coinflow',
   },
 ];
 
